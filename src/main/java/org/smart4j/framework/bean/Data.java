fbebb6e0ec13645a510c1f026c2030d55c6b1e11
sjp <shijiapeng@moko.cc>
2016-11-05 17:45:59 +0800

    增加构造函数

--- src/main/java/org/smart4j/framework/bean/Data.java
+++ src/main/java/org/smart4j/framework/bean/Data.java
@@ -8,11 +8,13 @@ package org.smart4j.framework.bean;
 public class Data {
     private Object model;
 
+    public Data(Object model) {
+        this.model = model;
+    }
+
     public Object getModel() {
         return model;
     }
 
-    public void setModel(Object model) {
-        this.model = model;
-    }
+
 }
